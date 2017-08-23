54c7907634545b8f6302ec638365b383808179ee
Ben Crouse <bcrouse@singlestoneconsulting.com>
2017-08-23 14:10:18 -0400

    tally component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TallyComponent } from './tally/tally.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TallyComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/tally/tally.ts
@@ -0,0 +1,4 @@
+export class TallyCharacter {
+    symbolType: string;
+    value: string;
+}
