fc234b46412ca949eb571b0ed5b0e3d9d3de49d3
Ben Crouse <crousky@hotmail.com>
2017-09-08 10:42:54 -0400

    project detail

--- src/app/models/project.ts
+++ src/app/models/project.ts
@@ -1,9 +1,16 @@
+import { Metric } from './metric';
+
 export class Project {
     id: string;
     projectName: string;
     clientName: string;
     completionDate: Date;
     solutionName: string;
+    description: string;
     peopleImpacted: number;
+    externalPeopleImpacted: Metric;
+    internalPeopleImpacted: Metric;
     valueCreated: number;
+    costReduction: Metric;
+    incrementalRevenue: Metric;
 }
