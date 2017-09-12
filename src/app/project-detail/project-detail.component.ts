04ece11a7990127c47821940100b51bc63466590
Ben Crouse <crousky@hotmail.com>
2017-09-12 13:20:38 -0400

    uxqa fixes

--- src/app/project-detail/project-detail.component.ts
+++ src/app/project-detail/project-detail.component.ts
@@ -14,7 +14,9 @@ import { Metric } from '../models/metric';
 export class ProjectDetailComponent implements OnInit {
   project: Project;
   peopleMetrics: ProjectMetric[];
+  totalPeople: number;
   valueMetrics: ProjectMetric[];
+  totalValue: number;
   metricDescription: string;
 
   constructor(
@@ -28,21 +30,27 @@ export class ProjectDetailComponent implements OnInit {
     this.route.paramMap.subscribe(p => {
       this.projectService.getProjects().subscribe(projects => {
         this.peopleMetrics = [];
+        this.totalPeople = 0;
         this.valueMetrics = [];
+        this.totalValue = 0;
         const filteredProjects: Project[] = projects.filter(x => x.id === p.get('id'));
         if (filteredProjects.length === 1) {
           this.project = filteredProjects[0];
           if (this.project.externalPeopleImpacted && this.project.externalPeopleImpacted.value > 0) {
             this.peopleMetrics.push(this.createProjectMetric('External', this.project.externalPeopleImpacted));
+            this.totalPeople += this.project.externalPeopleImpacted.value;
           }
           if (this.project.internalPeopleImpacted && this.project.internalPeopleImpacted.value > 0) {
             this.peopleMetrics.push(this.createProjectMetric('Internal', this.project.internalPeopleImpacted));
+            this.totalPeople += this.project.internalPeopleImpacted.value;
           }
           if (this.project.costReduction && this.project.costReduction.value > 0) {
             this.valueMetrics.push(this.createProjectMetric('Cost Reduction', this.project.costReduction));
+            this.totalValue += this.project.costReduction.value;
           }
           if (this.project.incrementalRevenue && this.project.incrementalRevenue.value > 0) {
             this.valueMetrics.push(this.createProjectMetric('Incremental Revenue', this.project.incrementalRevenue));
+            this.totalValue += this.project.incrementalRevenue.value;
           }
           if (this.peopleMetrics.length > 0) {
             this.peopleMetrics[0].selected = true;
